ed9c835e9d51e842e5ce45c1252526fe6eb30da2
bala-amypo <bala@amypo.in>
2025-12-17 08:59:40 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,10 @@
- package com.example.demo.Service;
- public class StudentService{
+package com.example.demo.service;
 
+import java.util.List;
+import com.example.demo.entity.StudentEntity;
+
+public interface StudentService {
+    public StudentEntity addStudents(StudentEntity student);
+    public List<StudentEntity> getStudents();
     
- }
+}
